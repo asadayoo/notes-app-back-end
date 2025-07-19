24d8d65ab511f9200a2ebb92679b9d1257d521d3
asadayoo <ahmadassadf428@gmail.com>
2025-07-19 14:18:03 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByidHandler, deleteNoteByIdHandler } = require('./handler');
+
+const routes = [
+ {
+   method: 'POST',
+   path: '/notes',
+   handler: addNoteHandler,
+ },
+ //Latihan: Menampilkan Catatan
+ {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+ },
+ //mendapatkan catatan secara spesifik
+ {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNoteByIdHandler,
+ },
+ {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNoteByidHandler,
+ }, 
+ {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteNoteByIdHandler,
+ },
+];
+ 
+module.exports = routes;

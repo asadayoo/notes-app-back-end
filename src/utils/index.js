6325d4f40f47b004b604cb1da4cbefeed06f54d2
asadayoo <ahmadassadf428@gmail.com>
2025-07-25 09:37:41 +0700

    postgres database implemented

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+//MAPPING - Tujuannya untuk memudahkan dalam penggunaan ulang.
+
+const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+  id,
+  title,
+  body,
+  tags,
+  createdAt: created_at,
+  updatedAt: updated_at,
+});
+
+module.exports = { mapDBToModel };

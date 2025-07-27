eeb882808b832e910e0330a9101a1bd2c0500a4a
asadayoo <ahmadassadf428@gmail.com>
2025-07-27 20:49:48 +0700

    user registration, authentication, authorization implemented

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,48 +1,49 @@
-const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
-
+const { nanoid } = require("nanoid");
 const InvariantError = require("../../exceptions/InvariantError");
-const { mapDBToModel } = require("../../utils");
 const NotFoundError = require("../../exceptions/NotFoundError");
+const AuthorizationError = require("../../exceptions/AuthorizationError");
+const { mapDBToModel } = require("../../utils/index");
 
 class NotesService {
   constructor() {
     this._pool = new Pool();
   }
-  //ADD NOTE
-  async addNote({ title, body, tags }) {
+
+  async addNote({ title, body, tags, owner }) {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
 
-    //QUERY
     const query = {
-      text: "INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id",
-      values: [id, title, body, tags, createdAt, updatedAt],
+      text: "INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING id",
+      values: [id, title, body, tags, createdAt, updatedAt, owner],
     };
 
-    //mengeksekusi query yang sudah dibuat
     const result = await this._pool.query(query);
 
-    //Untuk memastikan notes berhasil dimasukan ke database,
-    // kita bisa evaluasi nilai dari results.rows[0].id
-    // (karena kita melakukan returning id pada query).
     if (!result.rows[0].id) {
       throw new InvariantError("Catatan gagal ditambahkan");
     }
+
     return result.rows[0].id;
   }
-  //GET NOTE
-  async getNotes() {
-    const result = await this._pool.query("SELECT * FROM notes");
-    //Kalau hasil SELECT hanya satu baris, bisa langsung:
+
+  async getNotes(owner) {
+    const query = {
+      text: "SELECT * FROM notes WHERE owner = $1",
+      values: [owner],
+    };
+    const result = await this._pool.query(query);
     return result.rows.map(mapDBToModel);
   }
 
-  //GET BY ID
   async getNoteById(id) {
     const query = {
-      text: "SELECT * FROM notes WHERE id = $1",
+      text: `SELECT notes.*, users.username
+    FROM notes
+    LEFT JOIN users ON users.id = notes.owner
+    WHERE notes.id = $1`,
       values: [id],
     };
     const result = await this._pool.query(query);
@@ -54,7 +55,6 @@ class NotesService {
     return result.rows.map(mapDBToModel)[0];
   }
 
-  //UPDATE BY ID
   async editNoteById(id, { title, body, tags }) {
     const updatedAt = new Date().toISOString();
     const query = {
@@ -63,13 +63,12 @@ class NotesService {
     };
 
     const result = await this._pool.query(query);
+
     if (!result.rows.length) {
-      throw new NotFoundError(
-        "Gagal memperbaharui Catatan. ID tidak ditemukan"
-      );
+      throw new NotFoundError("Gagal memperbarui catatan. Id tidak ditemukan");
     }
   }
-  //DELETE BY ID
+
   async deleteNoteById(id) {
     const query = {
       text: "DELETE FROM notes WHERE id = $1 RETURNING id",
@@ -77,8 +76,24 @@ class NotesService {
     };
 
     const result = await this._pool.query(query);
+
     if (!result.rows.length) {
-      throw new NotFoundError("Catatan gagal dihapus. ID tidak ditemukan");
+      throw new NotFoundError("Catatan gagal dihapus. Id tidak ditemukan");
+    }
+  }
+
+  async verifyNoteOwner(id, owner) {
+    const query = {
+      text: "SELECT * FROM notes WHERE id = $1",
+      values: [id],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError("Catatan tidak ditemukan");
+    }
+    const note = result.rows[0];
+    if (note.owner !== owner) {
+      throw new AuthorizationError("Anda tidak berhak mengakses resource ini");
     }
   }
 }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,21 @@
 //MAPPING - Tujuannya untuk memudahkan dalam penggunaan ulang.
 
-const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  created_at,
+  updated_at,
+  username,
+}) => ({
   id,
   title,
   body,
   tags,
   createdAt: created_at,
   updatedAt: updated_at,
+  username,
 });
 
 module.exports = { mapDBToModel };

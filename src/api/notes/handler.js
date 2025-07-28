41c1e2106b50785788e39ac938f1991d3f60899a
asadayoo <ahmadassadf428@gmail.com>
2025-07-28 23:49:17 +0700

    collaboration implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -8,6 +8,7 @@ class NotesHandler {
     this.getNoteByIdHandler = this.getNoteByIdHandler.bind(this);
     this.putNoteByIdHandler = this.putNoteByIdHandler.bind(this);
     this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
+    this.getUsersByUsernameHandler = this.getUsersByUsernameHandler.bind(this);
   }
 
   async postNoteHandler(request, h) {
@@ -48,7 +49,7 @@ class NotesHandler {
     const { id } = request.params;
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyNoteOwner(id, credentialId);
+    await this._service.verifyNoteAccess(id, credentialId);
     const note = await this._service.getNoteById(id);
     return {
       status: "success",
@@ -63,9 +64,8 @@ class NotesHandler {
     const { id } = request.params;
     const { id: credentialId } = request.auth.credentials;
 
-    await this._service.verifyNoteOwner(id, credentialId);
-    this._service.editNoteById(id, request.payload);
-
+    await this._service.verifyNoteAccess(id, credentialId);
+    await this._service.editNoteById(id, request.payload);
     return {
       status: "success",
       message: "Catatan berhasil diperbarui",
@@ -82,6 +82,17 @@ class NotesHandler {
     return {
       status: "success",
       message: "Catatan berhasil dihapus",
+    };
+  }
+
+  async getUsersByUsernameHandler(request, h) {
+    const { username = "" } = request.query;
+    const users = await this._service.getUsersByUsername(username);
+    return {
+      status: "success",
+      data: {
+        users,
+      },
     };
   }
 }
